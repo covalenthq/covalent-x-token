3c3db2841fcd77415c42413feabe483f9b8a8881
Krish-vemula <152236536+Krish-vemula@users.noreply.github.com>
2024-04-08 16:00:54 -0700

    Added Permit2 testcases (#4)
    Co-authored-by: Krishnateja Vemula <krishnatejavemula@Krishnatejas-MacBook-Pro.local>

--- test/CovalentXToken.t.sol
+++ test/CovalentXToken.t.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-import {CovalentXToken} from "src/CovalentXToken.sol";
 import {ICovalentXToken} from "src/interfaces/ICovalentXToken.sol";
+import {IPermit2} from "src/interfaces/IPermit2.sol";
+import {CovalentXToken} from "src/CovalentXToken.sol";
 import {DefaultEmissionManager} from "src/DefaultEmissionManager.sol";
 import {TransparentUpgradeableProxy, ProxyAdmin} from "openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
 import {Test} from "forge-std/Test.sol";
@@ -17,8 +18,16 @@ contract CovalentXTokenTest is Test {
     address public emergencyCouncil;
     DefaultEmissionManager public emissionManager;
     uint256 public mintPerSecondCap = 2.5e18;
+    bytes32 public constant _PERMIT_SINGLE_TYPEHASH =
+        keccak256(
+            "PermitSingle(PermitDetails details,address spender,uint256 sigDeadline)PermitDetails(address token,uint160 amount,uint48 expiration,uint48 nonce)"
+        );
+    bytes32 public constant _PERMIT_DETAILS_TYPEHASH =
+        keccak256("PermitDetails(address token,uint160 amount,uint48 expiration,uint48 nonce)");
 
     function setUp() external {
+        uint256 forkId = vm.createFork(vm.envString("MAINNET_RPC_URL"), 19610930);
+        vm.selectFork(forkId);
         migration = makeAddr("migration");
         treasury = makeAddr("treasury");
         protocolCouncil = makeAddr("protocolCouncil");
@@ -146,5 +155,129 @@ contract CovalentXTokenTest is Test {
         CXT.mint(to, amount);
 
         if (amount <= maxMint) assertEq(CXT.balanceOf(to), amount);
+    }
+
+    function test_Permit2Transfer() external {
+        address user = makeAddr("user");
+        uint256 fromPrivateKey = 0x12341234;
+        address from = vm.addr(fromPrivateKey);
+        uint160 defaultAmount = 10 * 10e18;
+        uint48 defaultNonce = 0;
+        uint48 defaultExpiration = uint48(block.timestamp + 5);
+
+        IPermit2 permit2 = IPermit2(CXT.PERMIT2());
+        vm.prank(migration);
+        CXT.transfer(from, defaultAmount);
+        IPermit2.PermitSingle memory permit = defaultERC20PermitAllowance(
+            address(CXT),
+            defaultAmount,
+            defaultExpiration,
+            defaultNonce
+        );
+        bytes memory sig = getPermitSignature(permit, fromPrivateKey, permit2.DOMAIN_SEPARATOR());
+
+        uint256 startBalanceTo = CXT.balanceOf(user);
+        permit2.permit(from, permit, sig);
+
+        permit2.transferFrom(from, user, defaultAmount, address(CXT));
+        assertEq(CXT.balanceOf(user), startBalanceTo + defaultAmount);
+    }
+
+    function test_RevertExtraAmountPermit2Transfer() external {
+        address user = makeAddr("user");
+        uint256 fromPrivateKey = 0x12341234;
+        address from = vm.addr(fromPrivateKey);
+        uint160 defaultAmount = 10 * 10e18;
+        uint48 defaultNonce = 0;
+        uint48 defaultExpiration = uint48(block.timestamp + 5);
+        IPermit2 permit2 = IPermit2(CXT.PERMIT2());
+        vm.prank(migration);
+        CXT.transfer(from, defaultAmount);
+        IPermit2.PermitSingle memory permit = defaultERC20PermitAllowance(
+            address(CXT),
+            defaultAmount,
+            defaultExpiration,
+            defaultNonce
+        );
+        bytes memory sig = getPermitSignature(permit, fromPrivateKey, permit2.DOMAIN_SEPARATOR());
+
+        permit2.permit(from, permit, sig);
+
+        vm.expectRevert(abi.encodeWithSelector(IPermit2.InsufficientAllowance.selector, defaultAmount));
+        permit2.transferFrom(from, user, defaultAmount * 2, address(CXT));
+    }
+
+    function test_RevertPermit2DuplicateSigTransfer() external {
+        address user = makeAddr("user");
+        uint256 fromPrivateKey = 0x12341234;
+        address from = vm.addr(fromPrivateKey);
+        uint160 defaultAmount = 10 * 10e18;
+        uint48 defaultNonce = 0;
+        uint48 defaultExpiration = uint48(block.timestamp + 5);
+
+        IPermit2 permit2 = IPermit2(CXT.PERMIT2());
+        vm.prank(migration);
+        CXT.transfer(from, defaultAmount);
+        IPermit2.PermitSingle memory permit = defaultERC20PermitAllowance(
+            address(CXT),
+            defaultAmount,
+            defaultExpiration,
+            defaultNonce
+        );
+        bytes memory sig = getPermitSignature(permit, fromPrivateKey, permit2.DOMAIN_SEPARATOR());
+
+        uint256 startBalanceTo = CXT.balanceOf(user);
+        permit2.permit(from, permit, sig);
+
+        permit2.transferFrom(from, user, defaultAmount, address(CXT));
+        assertEq(CXT.balanceOf(user), startBalanceTo + defaultAmount);
+        vm.prank(migration);
+        CXT.transfer(from, defaultAmount);
+        vm.expectRevert(abi.encodeWithSelector(IPermit2.InsufficientAllowance.selector, 0));
+        permit2.transferFrom(from, user, defaultAmount, address(CXT));
+    }
+
+    // Permit2 helper functions
+
+    function getPermitSignatureRaw(
+        IPermit2.PermitSingle memory permit,
+        uint256 privateKey,
+        bytes32 domainSeparator
+    ) internal pure returns (uint8 v, bytes32 r, bytes32 s) {
+        bytes32 permitHash = keccak256(abi.encode(_PERMIT_DETAILS_TYPEHASH, permit.details));
+
+        bytes32 msgHash = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                domainSeparator,
+                keccak256(abi.encode(_PERMIT_SINGLE_TYPEHASH, permitHash, permit.spender, permit.sigDeadline))
+            )
+        );
+
+        (v, r, s) = vm.sign(privateKey, msgHash);
+    }
+
+    function getPermitSignature(
+        IPermit2.PermitSingle memory permit,
+        uint256 privateKey,
+        bytes32 domainSeparator
+    ) internal pure returns (bytes memory sig) {
+        (uint8 v, bytes32 r, bytes32 s) = getPermitSignatureRaw(permit, privateKey, domainSeparator);
+        return bytes.concat(r, s, bytes1(v));
+    }
+
+    function defaultERC20PermitAllowance(
+        address token0,
+        uint160 amount,
+        uint48 expiration,
+        uint48 nonce
+    ) internal view returns (IPermit2.PermitSingle memory) {
+        IPermit2.PermitDetails memory details = IPermit2.PermitDetails({
+            token: token0,
+            amount: amount,
+            expiration: expiration,
+            nonce: nonce
+        });
+        return IPermit2.PermitSingle({details: details, spender: address(this), sigDeadline: block.timestamp + 100});
     }
 }
